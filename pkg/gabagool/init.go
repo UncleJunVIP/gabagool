27537ae8c3bd975c550b1f132bd4d315d87b41cc
Brandon T. Kowalski <brandon@kowalski.io>
2025-05-21 01:54:53 -0400

    swap theme init

--- pkg/gabagool/init.go
+++ pkg/gabagool/init.go
@@ -9,8 +9,8 @@ type GabagoolOptions struct {
 // InitSDL initializes SDL and the UI
 // Must be called before any other UI functions!
 func InitSDL(options GabagoolOptions) {
-	Init(options.WindowTitle, options.ShowBackground)
 	initTheme()
+	Init(options.WindowTitle, options.ShowBackground)
 }
 
 // CloseSDL Tidies up SDL and the UI
