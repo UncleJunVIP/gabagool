dadaeb4d4a55e37b1584dec6fbf0f28046845ddb
Brandon T. Kowalski <brandon@kowalski.io>
2025-06-12 18:58:02 -0400

    Smaller range of color picker colors

--- pkg/gabagool/color_picker.go
+++ pkg/gabagool/color_picker.go
@@ -15,7 +15,7 @@ type ColorPicker struct {
 	GridRows        int32           // Number of rows in the grid
 	GridCols        int32           // Number of columns in the grid
 	SelectedIndex   int             // Currently selected color cell
-	Visible         bool            // Is the picker visible
+	Visible         bool            // Is the picker visible?
 	Colors          []sdl.Color     // Available colors in the picker
 	OnColorSelected func(sdl.Color) // Callback when a color is selected
 }
@@ -28,82 +28,46 @@ func NewHexColorPicker(window *Window) *ColorPicker {
 	size := int32(math.Min(float64(window.Width), float64(window.Height)) * 0.8) // 80% of screen
 
 	// Define grid dimensions
-	gridRows := int32(7)
-	gridCols := int32(7)
+	gridRows := int32(5)
+	gridCols := int32(5)
 	cellSize := size / (int32(math.Max(float64(gridRows), float64(gridCols))) + 1)
 	cellPadding := int32(4)
 
-	// Create an extended color palette with at least 48 colors
+	// Initialize with 25 bold, highly distinguishable colors
 	colors := []sdl.Color{
-		// Reds
-		{R: 255, G: 0, B: 0, A: 255},     // Red
-		{R: 220, G: 20, B: 60, A: 255},   // Crimson
-		{R: 178, G: 34, B: 34, A: 255},   // Firebrick
-		{R: 139, G: 0, B: 0, A: 255},     // Dark red
-		{R: 255, G: 99, B: 71, A: 255},   // Tomato
-		{R: 205, G: 92, B: 92, A: 255},   // Indian red
-		{R: 240, G: 128, B: 128, A: 255}, // Light coral
+		// Row 1: Primary colors and variants
+		{R: 255, G: 0, B: 0, A: 255},   // Red
+		{R: 0, G: 255, B: 0, A: 255},   // Green
+		{R: 0, G: 0, B: 255, A: 255},   // Blue
+		{R: 255, G: 255, B: 0, A: 255}, // Yellow
+		{R: 255, G: 0, B: 255, A: 255}, // Magenta
 
-		// Oranges
-		{R: 255, G: 127, B: 0, A: 255}, // Orange
-		{R: 255, G: 140, B: 0, A: 255}, // Dark orange
-		{R: 255, G: 165, B: 0, A: 255}, // Orange
-		{R: 255, G: 69, B: 0, A: 255},  // Orange red
+		// Row 2: Secondary colors and variants
+		{R: 0, G: 255, B: 255, A: 255}, // Cyan
+		{R: 255, G: 128, B: 0, A: 255}, // Orange
+		{R: 128, G: 0, B: 255, A: 255}, // Purple
+		{R: 0, G: 128, B: 0, A: 255},   // Dark Green
+		{R: 128, G: 0, B: 0, A: 255},   // Maroon
 
-		// Yellows
-		{R: 255, G: 255, B: 0, A: 255},   // Yellow
-		{R: 255, G: 215, B: 0, A: 255},   // Gold
-		{R: 218, G: 165, B: 32, A: 255},  // Goldenrod
-		{R: 240, G: 230, B: 140, A: 255}, // Khaki
+		// Row 3: Tertiary colors
+		{R: 0, G: 0, B: 128, A: 255},     // Navy
+		{R: 0, G: 128, B: 128, A: 255},   // Teal
+		{R: 128, G: 128, B: 0, A: 255},   // Olive
+		{R: 128, G: 0, B: 128, A: 255},   // Purple
+		{R: 255, G: 128, B: 128, A: 255}, // Pink
 
-		// Greens
-		{R: 0, G: 128, B: 0, A: 255},     // Green
-		{R: 34, G: 139, B: 34, A: 255},   // Forest green
-		{R: 0, G: 255, B: 0, A: 255},     // Lime
-		{R: 50, G: 205, B: 50, A: 255},   // Lime green
-		{R: 144, G: 238, B: 144, A: 255}, // Light green
-		{R: 152, G: 251, B: 152, A: 255}, // Pale green
-		{R: 143, G: 188, B: 143, A: 255}, // Dark sea green
+		// Row 4: Bright variants
+		{R: 255, G: 192, B: 0, A: 255},   // Gold
+		{R: 128, G: 255, B: 0, A: 255},   // Lime
+		{R: 0, G: 128, B: 255, A: 255},   // Sky Blue
+		{R: 255, G: 0, B: 128, A: 255},   // Rose
+		{R: 128, G: 255, B: 255, A: 255}, // Light Cyan
 
-		// Cyans
-		{R: 0, G: 255, B: 255, A: 255},  // Cyan
-		{R: 0, G: 139, B: 139, A: 255},  // Dark cyan
-		{R: 32, G: 178, B: 170, A: 255}, // Light sea green
-		{R: 64, G: 224, B: 208, A: 255}, // Turquoise
-
-		// Blues
-		{R: 0, G: 0, B: 255, A: 255},     // Blue
-		{R: 0, G: 0, B: 139, A: 255},     // Dark blue
-		{R: 0, G: 0, B: 205, A: 255},     // Medium blue
-		{R: 65, G: 105, B: 225, A: 255},  // Royal blue
-		{R: 100, G: 149, B: 237, A: 255}, // Cornflower blue
-		{R: 135, G: 206, B: 235, A: 255}, // Sky blue
-		{R: 135, G: 206, B: 250, A: 255}, // Light sky blue
-
-		// Purples
-		{R: 128, G: 0, B: 128, A: 255},  // Purple
-		{R: 148, G: 0, B: 211, A: 255},  // Dark violet
-		{R: 153, G: 50, B: 204, A: 255}, // Dark orchid
-		{R: 186, G: 85, B: 211, A: 255}, // Medium orchid
-		{R: 138, G: 43, B: 226, A: 255}, // Blue violet
-		{R: 75, G: 0, B: 130, A: 255},   // Indigo
-
-		// Pinks
-		{R: 255, G: 192, B: 203, A: 255}, // Pink
-		{R: 255, G: 105, B: 180, A: 255}, // Hot pink
-		{R: 219, G: 112, B: 147, A: 255}, // Pale violet red
-		{R: 199, G: 21, B: 133, A: 255},  // Medium violet red
-
-		// Browns
-		{R: 165, G: 42, B: 42, A: 255},  // Brown
-		{R: 139, G: 69, B: 19, A: 255},  // Saddle brown
-		{R: 160, G: 82, B: 45, A: 255},  // Sienna
-		{R: 210, G: 105, B: 30, A: 255}, // Chocolate
-
-		// Neutrals
+		// Row 5: Grayscale + special colors
 		{R: 255, G: 255, B: 255, A: 255}, // White
 		{R: 192, G: 192, B: 192, A: 255}, // Silver
 		{R: 128, G: 128, B: 128, A: 255}, // Gray
+		{R: 64, G: 64, B: 64, A: 255},    // Dark Gray
 		{R: 0, G: 0, B: 0, A: 255},       // Black
 	}
 
@@ -315,173 +279,47 @@ func (h *ColorPicker) SetOnColorSelected(callback func(sdl.Color)) {
 	h.OnColorSelected = callback
 }
 
+// InitColors initializes the color picker with 25 bold, distinct colors for retro gaming
 func (h *ColorPicker) InitColors() {
-	// Initialize with the specific hex colors provided
+	// Set grid dimensions to accommodate 25 colors (5x5 grid)
+	h.GridRows = 5
+	h.GridCols = 5
+
+	// Initialize with 25 bold, highly distinguishable colors
 	h.Colors = []sdl.Color{
-		// Blues
-		{R: 0x00, G: 0x00, B: 0x11, A: 255},
-		{R: 0x00, G: 0x00, B: 0x22, A: 255},
-		{R: 0x00, G: 0x00, B: 0x33, A: 255},
-		{R: 0x00, G: 0x00, B: 0x44, A: 255},
-		{R: 0x00, G: 0x00, B: 0x55, A: 255},
-		{R: 0x00, G: 0x00, B: 0x66, A: 255},
-		{R: 0x00, G: 0x00, B: 0x77, A: 255},
-		{R: 0x00, G: 0x00, B: 0x88, A: 255},
-		{R: 0x00, G: 0x00, B: 0x99, A: 255},
-		{R: 0x00, G: 0x00, B: 0xAA, A: 255},
-		{R: 0x00, G: 0x00, B: 0xBB, A: 255},
-		{R: 0x00, G: 0x00, B: 0xCC, A: 255},
-		{R: 0x33, G: 0x66, B: 0xFF, A: 255},
-		{R: 0x4D, G: 0x7A, B: 0xFF, A: 255},
-		{R: 0x66, G: 0x99, B: 0xFF, A: 255},
-		{R: 0x80, G: 0xB3, B: 0xFF, A: 255},
-		{R: 0x99, G: 0xCC, B: 0xFF, A: 255},
-		{R: 0xB3, G: 0xD9, B: 0xFF, A: 255},
-		{R: 0x00, G: 0x00, B: 0xFF, A: 255},
+		// Row 1: Primary colors and variants
+		{R: 255, G: 0, B: 0, A: 255},   // Red
+		{R: 0, G: 255, B: 0, A: 255},   // Green
+		{R: 0, G: 0, B: 255, A: 255},   // Blue
+		{R: 255, G: 255, B: 0, A: 255}, // Yellow
+		{R: 255, G: 0, B: 255, A: 255}, // Magenta
 
-		// Cyan
-		{R: 0x00, G: 0x11, B: 0x11, A: 255},
-		{R: 0x00, G: 0x22, B: 0x22, A: 255},
-		{R: 0x00, G: 0x33, B: 0x33, A: 255},
-		{R: 0x00, G: 0x44, B: 0x44, A: 255},
-		{R: 0x00, G: 0x55, B: 0x55, A: 255},
-		{R: 0x00, G: 0x66, B: 0x66, A: 255},
-		{R: 0x00, G: 0x77, B: 0x77, A: 255},
-		{R: 0x00, G: 0x88, B: 0x88, A: 255},
-		{R: 0x00, G: 0x99, B: 0x99, A: 255},
-		{R: 0x00, G: 0xAA, B: 0xAA, A: 255},
-		{R: 0x00, G: 0xBB, B: 0xBB, A: 255},
-		{R: 0x00, G: 0xCC, B: 0xCC, A: 255},
-		{R: 0x33, G: 0xFF, B: 0xFF, A: 255},
-		{R: 0x4D, G: 0xFF, B: 0xFF, A: 255},
-		{R: 0x66, G: 0xFF, B: 0xFF, A: 255},
-		{R: 0x80, G: 0xFF, B: 0xFF, A: 255},
-		{R: 0x99, G: 0xFF, B: 0xFF, A: 255},
-		{R: 0xB3, G: 0xFF, B: 0xFF, A: 255},
-		{R: 0x00, G: 0xFF, B: 0xFF, A: 255},
+		// Row 2: Secondary colors and variants
+		{R: 0, G: 255, B: 255, A: 255}, // Cyan
+		{R: 255, G: 128, B: 0, A: 255}, // Orange
+		{R: 128, G: 0, B: 255, A: 255}, // Purple
+		{R: 0, G: 128, B: 0, A: 255},   // Dark Green
+		{R: 128, G: 0, B: 0, A: 255},   // Maroon
 
-		// Green
-		{R: 0x00, G: 0x11, B: 0x00, A: 255},
-		{R: 0x00, G: 0x22, B: 0x00, A: 255},
-		{R: 0x00, G: 0x33, B: 0x00, A: 255},
-		{R: 0x00, G: 0x44, B: 0x00, A: 255},
-		{R: 0x00, G: 0x55, B: 0x00, A: 255},
-		{R: 0x00, G: 0x66, B: 0x00, A: 255},
-		{R: 0x00, G: 0x77, B: 0x00, A: 255},
-		{R: 0x00, G: 0x88, B: 0x00, A: 255},
-		{R: 0x00, G: 0x99, B: 0x00, A: 255},
-		{R: 0x00, G: 0xAA, B: 0x00, A: 255},
-		{R: 0x00, G: 0xBB, B: 0x00, A: 255},
-		{R: 0x00, G: 0xCC, B: 0x00, A: 255},
-		{R: 0x33, G: 0xFF, B: 0x33, A: 255},
-		{R: 0x4D, G: 0xFF, B: 0x4D, A: 255},
-		{R: 0x66, G: 0xFF, B: 0x66, A: 255},
-		{R: 0x80, G: 0xFF, B: 0x80, A: 255},
-		{R: 0x99, G: 0xFF, B: 0x99, A: 255},
-		{R: 0xB3, G: 0xFF, B: 0xB3, A: 255},
-		{R: 0x00, G: 0xFF, B: 0x00, A: 255},
+		// Row 3: Tertiary colors
+		{R: 0, G: 0, B: 128, A: 255},     // Navy
+		{R: 0, G: 128, B: 128, A: 255},   // Teal
+		{R: 128, G: 128, B: 0, A: 255},   // Olive
+		{R: 128, G: 0, B: 128, A: 255},   // Purple
+		{R: 255, G: 128, B: 128, A: 255}, // Pink
 
-		// Magenta
-		{R: 0x11, G: 0x00, B: 0x11, A: 255},
-		{R: 0x22, G: 0x00, B: 0x22, A: 255},
-		{R: 0x33, G: 0x00, B: 0x33, A: 255},
-		{R: 0x44, G: 0x00, B: 0x44, A: 255},
-		{R: 0x55, G: 0x00, B: 0x55, A: 255},
-		{R: 0x66, G: 0x00, B: 0x66, A: 255},
-		{R: 0x77, G: 0x00, B: 0x77, A: 255},
-		{R: 0x88, G: 0x00, B: 0x88, A: 255},
-		{R: 0x99, G: 0x00, B: 0x99, A: 255},
-		{R: 0xAA, G: 0x00, B: 0xAA, A: 255},
-		{R: 0xBB, G: 0x00, B: 0xBB, A: 255},
-		{R: 0xCC, G: 0x00, B: 0xCC, A: 255},
-		{R: 0xFF, G: 0x33, B: 0xFF, A: 255},
-		{R: 0xFF, G: 0x4D, B: 0xFF, A: 255},
-		{R: 0xFF, G: 0x66, B: 0xFF, A: 255},
-		{R: 0xFF, G: 0x80, B: 0xFF, A: 255},
-		{R: 0xFF, G: 0x99, B: 0xFF, A: 255},
-		{R: 0xFF, G: 0xB3, B: 0xFF, A: 255},
-		{R: 0xFF, G: 0x00, B: 0xFF, A: 255},
+		// Row 4: Bright variants
+		{R: 255, G: 192, B: 0, A: 255},   // Gold
+		{R: 128, G: 255, B: 0, A: 255},   // Lime
+		{R: 0, G: 128, B: 255, A: 255},   // Sky Blue
+		{R: 255, G: 0, B: 128, A: 255},   // Rose
+		{R: 128, G: 255, B: 255, A: 255}, // Light Cyan
 
-		// Purple
-		{R: 0x22, G: 0x00, B: 0x44, A: 255},
-		{R: 0x33, G: 0x00, B: 0x66, A: 255},
-		{R: 0x44, G: 0x00, B: 0x88, A: 255},
-		{R: 0x55, G: 0x00, B: 0xAA, A: 255},
-		{R: 0x66, G: 0x00, B: 0xCC, A: 255},
-		{R: 0x77, G: 0x00, B: 0xDD, A: 255},
-		{R: 0x88, G: 0x00, B: 0xEE, A: 255},
-		{R: 0x99, G: 0x00, B: 0xFF, A: 255},
-		{R: 0xAA, G: 0x00, B: 0xFF, A: 255},
-		{R: 0xBB, G: 0x00, B: 0xFF, A: 255},
-		{R: 0xCC, G: 0x00, B: 0xFF, A: 255},
-		{R: 0x88, G: 0x33, B: 0xFF, A: 255},
-		{R: 0x99, G: 0x4D, B: 0xFF, A: 255},
-		{R: 0xAA, G: 0x66, B: 0xFF, A: 255},
-		{R: 0xBB, G: 0x80, B: 0xFF, A: 255},
-		{R: 0xCC, G: 0x99, B: 0xFF, A: 255},
-		{R: 0xDD, G: 0xB3, B: 0xFF, A: 255},
-
-		// Red
-		{R: 0x22, G: 0x00, B: 0x00, A: 255},
-		{R: 0x44, G: 0x00, B: 0x00, A: 255},
-		{R: 0x66, G: 0x00, B: 0x00, A: 255},
-		{R: 0x88, G: 0x00, B: 0x00, A: 255},
-		{R: 0xAA, G: 0x00, B: 0x00, A: 255},
-		{R: 0xCC, G: 0x00, B: 0x00, A: 255},
-		{R: 0xFF, G: 0x33, B: 0x33, A: 255},
-		{R: 0xFF, G: 0x4D, B: 0x4D, A: 255},
-		{R: 0xFF, G: 0x66, B: 0x66, A: 255},
-		{R: 0xFF, G: 0x80, B: 0x80, A: 255},
-		{R: 0xFF, G: 0x99, B: 0x99, A: 255},
-		{R: 0xFF, G: 0xB3, B: 0xB3, A: 255},
-		{R: 0xFF, G: 0x00, B: 0x00, A: 255},
-
-		// Yellow
-		{R: 0x22, G: 0x22, B: 0x00, A: 255},
-		{R: 0x44, G: 0x44, B: 0x00, A: 255},
-		{R: 0x66, G: 0x66, B: 0x00, A: 255},
-		{R: 0x88, G: 0x88, B: 0x00, A: 255},
-		{R: 0xAA, G: 0xAA, B: 0x00, A: 255},
-		{R: 0xCC, G: 0xCC, B: 0x00, A: 255},
-		{R: 0xFF, G: 0xFF, B: 0x33, A: 255},
-		{R: 0xFF, G: 0xFF, B: 0x4D, A: 255},
-		{R: 0xFF, G: 0xFF, B: 0x66, A: 255},
-		{R: 0xFF, G: 0xFF, B: 0x80, A: 255},
-		{R: 0xFF, G: 0xFF, B: 0x99, A: 255},
-		{R: 0xFF, G: 0xFF, B: 0xB3, A: 255},
-		{R: 0xFF, G: 0xFF, B: 0x00, A: 255},
-
-		// Orange
-		{R: 0x33, G: 0x11, B: 0x00, A: 255},
-		{R: 0x66, G: 0x22, B: 0x00, A: 255},
-		{R: 0x99, G: 0x33, B: 0x00, A: 255},
-		{R: 0xCC, G: 0x44, B: 0x00, A: 255},
-		{R: 0xFF, G: 0x55, B: 0x00, A: 255},
-		{R: 0xFF, G: 0x66, B: 0x00, A: 255},
-		{R: 0xFF, G: 0x77, B: 0x11, A: 255},
-		{R: 0xFF, G: 0x88, B: 0x22, A: 255},
-		{R: 0xFF, G: 0x99, B: 0x33, A: 255},
-		{R: 0xFF, G: 0xAA, B: 0x44, A: 255},
-		{R: 0xFF, G: 0xBB, B: 0x55, A: 255},
-		{R: 0xFF, G: 0xCC, B: 0x66, A: 255},
-		{R: 0xFF, G: 0xDD, B: 0x77, A: 255},
-		{R: 0xFF, G: 0xEE, B: 0x88, A: 255},
-
-		// White to Black Gradient
-		{R: 0x00, G: 0x00, B: 0x00, A: 255},
-		{R: 0x11, G: 0x11, B: 0x11, A: 255},
-		{R: 0x22, G: 0x22, B: 0x22, A: 255},
-		{R: 0x33, G: 0x33, B: 0x33, A: 255},
-		{R: 0x44, G: 0x44, B: 0x44, A: 255},
-		{R: 0x55, G: 0x55, B: 0x55, A: 255},
-		{R: 0x66, G: 0x66, B: 0x66, A: 255},
-		{R: 0x77, G: 0x77, B: 0x77, A: 255},
-		{R: 0x88, G: 0x88, B: 0x88, A: 255},
-		{R: 0x99, G: 0x99, B: 0x99, A: 255},
-		{R: 0xAA, G: 0xAA, B: 0xAA, A: 255},
-		{R: 0xBB, G: 0xBB, B: 0xBB, A: 255},
-		{R: 0xCC, G: 0xCC, B: 0xCC, A: 255},
-		{R: 0xDD, G: 0xDD, B: 0xDD, A: 255},
-		{R: 0xFF, G: 0xFF, B: 0xFF, A: 255},
+		// Row 5: Grayscale + special colors
+		{R: 255, G: 255, B: 255, A: 255}, // White
+		{R: 192, G: 192, B: 192, A: 255}, // Silver
+		{R: 128, G: 128, B: 128, A: 255}, // Gray
+		{R: 64, G: 64, B: 64, A: 255},    // Dark Gray
+		{R: 0, G: 0, B: 0, A: 255},       // Black
 	}
 }
