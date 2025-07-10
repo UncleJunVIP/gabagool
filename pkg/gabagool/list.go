e642ef9d7c9306a5727d2b544d735480c89d9519
Brandon T. Kowalski <brandon@kowalski.io>
2025-07-10 01:02:46 -0400

    Still tuning repeat delay

--- pkg/gabagool/list.go
+++ pkg/gabagool/list.go
@@ -222,7 +222,7 @@ func newListController(options ListOptions) *listController {
 		itemScrollData:    make(map[int]*textScrollData),
 		titleScrollData:   &textScrollData{},
 		lastRepeatTime:    time.Now(),
-		repeatDelay:       100 * time.Millisecond,
+		repeatDelay:       150 * time.Millisecond,
 		repeatInterval:    0 * time.Millisecond,
 	}
 }
