689b6cd2f9aa5ab10561da61dc31d5d09e7c9192
Brandon T. Kowalski <brandon@kowalski.io>
2025-09-08 21:14:20 -0400

    Restore font scale

--- pkg/gabagool/functions.go
+++ pkg/gabagool/functions.go
@@ -3,8 +3,8 @@ package gabagool
 func getFontScale(width, height int32) int {
 
 	if width == DefaultWindowWidth && height == DefaultWindowHeight {
-		return 4
+		return 3
 	}
 
-	return 3
+	return 2
 }
